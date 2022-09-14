e33dc4267ec4670ab3887f179192f227d7658e4c
amcdonnelltemple <tuq80041@temple.edu>
2022-09-14 12:34:49 -0400

    replaced file

--- Rpractice.R
+++ Rpractice.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 ggplot(data = mpg) +
-  geom_point(mapping = aes(x =displ, y=hwy, color = class))
+  geom_point(mapping = aes(x =displ, y=hwy, shape = class))
 update.packages(ask = FALSE, checkBuilt = TRUE)
 
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x =displ, y=hwy),color = "green")
